2a3a16001fd18ee2c98b4badcc452cb493c34b8b
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2022-09-04 09:21:37 +0600

    news detail show in modal completed

--- js/app.js
+++ js/app.js
@@ -13,9 +13,8 @@ const loadCategory = async () =>{
 }
 
 const displayCategory = (categories) =>{
-    //console.log(categories)
+    
     categories.forEach(category => {
-        //console.log(category.category_id);
         const categoryList = document.getElementById('category-list');
         const list = document.createElement('li');
         list.classList.add('nav-item');
@@ -34,55 +33,100 @@ loadCategory();
 const categoryNewsById = async category_id =>{
 
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayCategoryNews(data.data);
-   
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        displayCategoryNews(data.data);
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
 
 const displayCategoryNews = data =>{
-    //console.log(data);
+    
     const newsContainer = document.getElementById('news-container');
     newsContainer.innerHTML = '';
     
     data.forEach(news =>{
-       // console.log(news);
+       
         const singleNews = document.createElement('div');
-        singleNews.classList.add('row', 'g-0');
+        singleNews.classList.add('card', 'm-5');
         singleNews.innerHTML =
 
         `
-            <div class="col-md-3">
-                <img src="${news.image_url}" class="img-fluid rounded-start" alt="">
-            </div>
-            <div class="col-md-9">
-                <div class="card-body">
-                    <h5 class="card-title">${news.title}</h5>
-                    <p class="card-text">${news.details}</p>
+            <div class ="row g-0">
+                <div class="col-md-3">
+                    <img src="${news ? news.thumbnail_url : 'Image not found'}" class="img-fluid rounded-start" alt="">
                 </div>
-                <div class="row col-md-12 p-3">
-                    <div class="row col-md-4">
-                        <div class="col-md-3">
-                            <img src="${news.author.img}" alt="" class="img-thumbnail">
+                <div class="col-md-9">
+                    <div class="card-body">
+                        <h5 class="card-title">${news ? news.title : 'Title not found'}</h5>
+                        <p id="news-description" class="card-text">${news ? news.details : 'No description available'}</p>
+                    </div>
+                    <div class="row col-md-12 p-3">
+                        <div class="row col-md-4">
+                            <div class="col-md-3">
+                                <img src="${news.author ? news.author.img : 'Author image not found'}" alt="" class="img-thumbnail">
+                            </div>
+                            <div class="col-md-9">
+                                <p class="m-0"><b>${news.author ? news.author.name : 'Author not found'}</b></p>
+                                <p class="m-0">${news.author ? news.author.published_date : 'Published date not available'}</p>
+                            </div>
                         </div>
-                        <div class="col-md-9">
-                            <p class="m-0"><b>${news.author.name}</b></p>
-                            <p class="m-0">${news.author.published_date}</p>
+                        <div class="col-md-2">
+                            <p><span><i class="fa-regular fa-eye"></i></span> ${news ? news.total_view : 'Data not found'}</p>
                         </div>
-                    </div>
-                    <div class="col-md-2">
-                        <p><span><i class="fa-regular fa-eye"></i></span> ${news.total_view}</p>
-                    </div>
-                    <div class="col-md-4">
-                        <p>${news.rating.number}</p>
-                    </div>
-                    <div class="col-md-2">
-                        <p class="text-primary fs-5"><span><i class="fa-solid fa-arrow-right"></i></span></p>
+                        <div class="col-md-4">
+                            <p>${news.rating ? news.rating.number : 'Data not available'}</p>
+                        </div>
+                        <div class="col-md-2">
+                            <p onclick="newsDetailsById('${news._id}')" class="text-primary fs-5" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <span><i class="fa-solid fa-arrow-right"></i></span>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </div>    
         `;
         newsContainer.appendChild(singleNews);  
     })
 }
 
+const newsDetailsById = async news_id =>{
+
+    const url =  `https://openapi.programming-hero.com/api/news/${news_id}`;
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        displayNewsModal(data.data);
+
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+const displayNewsModal = data =>{
+
+    const modalTitle = document.getElementById('newsDetailModalLabel');
+    modalTitle.innerText = data[0] ? data[0].title : 'Title not found' ;
+
+    const modalBodySection = document.getElementById('modal-body');
+    modalBodySection.innerHTML = '';
+    const modalBody = document.createElement('div');
+    modalBody.classList.add('card');
+    modalBody.innerHTML = 
+    
+    `
+        <img src="${data[0] ? data[0].image_url : 'Image not found'}" class="card-img-top" alt="">
+        <div class="card-body">
+            <h5 class="card-title">Author: ${data[0].author ? data[0].author.name : 'Author not found'}</h5>
+            <img src="${data[0].author ? data[0].author.img : 'Author image not found'}" alt="" class="modal-author-image">
+            <p class="card-text">Published Date: ${data[0].author ? data[0].author.published_date : 'Date not found'}</p>
+            <p class="card-text">${data[0] ? data[0].details : 'Description not found'}</p>
+            <p class="card-text">Total View: ${data[0] ? data[0].total_view : 'Data not found'}</p>
+        </div>
+    `;
+    modalBodySection.appendChild(modalBody);
+}
